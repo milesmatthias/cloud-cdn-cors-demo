feea9c5621e1f73f3be45a65de6346f6b35000d9
Miles Matthias <mmatthias@google.com>
2019-01-21 15:55:38 -0700

    wip on showing CORS headers.. waiting..

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -2,15 +2,32 @@
 
 PROJECT=$(gcloud config get-value project)
 LB=$PROJECT-demo-cdn-lb-ip-1
+BUCKET=$PROJECT-demo-cdn-origin
 
-IP_ADDR=$(gcloud compute addresses describe $LB --global | \
-  grep -e "^address:" | awk -F' ' '{print $2}')
+HEADERS=$(curl -vvvv -I -H "Origin: http://example.appspot.com" \
+  http://$BUCKET.storage.googleapis.com/index.html)
 
-
-# TODO -- loop while status == 404; takes CDN a bit to get up and running
-
-HEADERS=$(curl -vvvv -H "Host: example.appspot.com" $IP_ADDR/index.html)
+# HEADERS=$(curl -vvvv -H "Host: example.appspot.com" \
+#   -H "Access-Control-Request-Method: GET" \
+#   -H "Access-Control-Request-Headers: Content-Type" \
+#   -X OPTIONS \
+#   http://$BUCKET.storage.googleapis.com/index.html)
+#   #http://storage.googleapis.com/$BUCKET/index.html)
 
 echo $HEADERS
 
+# IP_ADDR=$(gcloud compute addresses describe $LB --global | \
+#   grep -e "^address:" | awk -F' ' '{print $2}')
+
+# TODO -- loop while status == 404; takes CDN a bit to get up and running
+
+# HEADERS=$(curl -vvvv -H "Host: example.appspot.com" \
+#   -H "Access-Control-Request-Method: POST" \
+#   -H "Access-Control-Request-Headers: X-Requested-With" \
+#   -X OPTIONS \
+#   $IP_ADDR/index.html)
+# 
+# echo $HEADERS
+
 # TODO -- give success or failure message
+# TODO -- saw note on SO that this may take 2-3 hours for CORS to propagate
