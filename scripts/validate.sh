d4e82b0b2394c0aff6567052cbcdb47612ee0209
Miles Matthias <mmatthias@google.com>
2019-01-22 11:25:09 -0700

    have validate working correctly, missing origin header

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -4,30 +4,42 @@ PROJECT=$(gcloud config get-value project)
 LB=$PROJECT-demo-cdn-lb-ip-1
 BUCKET=$PROJECT-demo-cdn-origin
 
-HEADERS=$(curl -vvvv -I -H "Origin: http://example.appspot.com" \
-  http://$BUCKET.storage.googleapis.com/index.html)
-
-# HEADERS=$(curl -vvvv -H "Host: example.appspot.com" \
-#   -H "Access-Control-Request-Method: GET" \
-#   -H "Access-Control-Request-Headers: Content-Type" \
-#   -X OPTIONS \
-#   http://$BUCKET.storage.googleapis.com/index.html)
-#   #http://storage.googleapis.com/$BUCKET/index.html)
-
-echo $HEADERS
-
-# IP_ADDR=$(gcloud compute addresses describe $LB --global | \
-#   grep -e "^address:" | awk -F' ' '{print $2}')
+IP_ADDR=$(gcloud compute addresses describe $LB --global | \
+  grep -e "^address:" | awk -F' ' '{print $2}')
 
 # TODO -- loop while status == 404; takes CDN a bit to get up and running
+HEADERS=$(curl -vvvv -I -H "Origin: http://example.appspot.com" \
+	$IP_ADDR/index.html)
 
-# HEADERS=$(curl -vvvv -H "Host: example.appspot.com" \
+# give success or failure message
+
+if [[ ! $HEADERS =~ "Access-Control-Allow-Origin: *" ]]; then
+	echo -e "FAILED: Missing the Access-Control-Allow-Origin header!"
+	echo -e "NOTE that it may take time to propogate these headers."
+else
+	echo "SUCCESS: 'Access-Control-Allow-Origin' header is present"
+fi
+
+if [[ ! $HEADERS =~ "Access-Control-Expose-Headers: Origin" ]]; then
+	echo -e "FAILED: Missing the Access-Control-Expose-Headers header!"
+	echo -e "NOTE that it may take time to propogate these headers."
+else
+	echo "SUCCESS: 'Access-Control-Expose-Headers' header is present"
+fi
+
+
+### Other ways to check CORS HEADERS
+
+# Option request
+# HEADERS=$(curl -vvvv -H "Origin: http://example.appspot.com" \
 #   -H "Access-Control-Request-Method: POST" \
 #   -H "Access-Control-Request-Headers: X-Requested-With" \
 #   -X OPTIONS \
 #   $IP_ADDR/index.html)
-# 
 # echo $HEADERS
 
-# TODO -- give success or failure message
-# TODO -- saw note on SO that this may take 2-3 hours for CORS to propagate
+# curl the GCS bucket directly
+# HEADERS=$(curl -vvvv -I -H "Origin: http://example.appspot.com" \
+#   http://$BUCKET.storage.googleapis.com/index.html)
+# echo $HEADERS
+
